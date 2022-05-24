a252e335d620f915799b2228c55a1b0024a475da
shrehanhalder <85818303+shrehanhalder@users.noreply.github.com>
2022-05-24 18:45:34 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCe_1GxIVpULE4qlQhen_nT5B9dmdCZ36U",
+  authDomain: "storytelling-abef8.firebaseapp.com",
+  databaseURL: "https://storytelling-abef8-default-rtdb.firebaseio.com",
+  projectId: "storytelling-abef8",
+  storageBucket: "storytelling-abef8.appspot.com",
+  messagingSenderId: "743935793528",
+  appId: "1:743935793528:web:7f2bf8aadd6fa337e7108d"
+};
